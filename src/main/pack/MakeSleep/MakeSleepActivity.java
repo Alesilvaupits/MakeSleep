90de530e313316f540e4f48bd004979eeba75520
alesilvaupits <alesilvaupits@gmail.com>
2011-11-10 00:42:35 -0200

    first commit

---
+++ src/main/pack/MakeSleep/MakeSleepActivity.java
@@ -0,0 +1,13 @@
+package main.pack.MakeSleep;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MakeSleepActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
