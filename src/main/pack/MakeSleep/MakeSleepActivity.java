0d3bd6eeea2611dc497b2175b69ac998b943cd7b
AlexUpis <alex.upits@gmail.com>
2011-11-13 10:41:14 -0200

    Implemtado o player da musica

--- src/main/pack/MakeSleep/MakeSleepActivity.java
+++ src/main/pack/MakeSleep/MakeSleepActivity.java
@@ -1,13 +1,30 @@
 package main.pack.MakeSleep;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MakeSleepActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
+	private  MediaPlayer player;
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        try {
+        	player = MediaPlayer.create(this, R.raw.raim);
+        	player.start();
+        	player.setLooping(true);
+        
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+		}
     }
+	@Override
+	public void onPause() {
+		player.setLooping(false);
+		player.stop();
+	}
+	
 }
